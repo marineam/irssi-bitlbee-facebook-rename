f43629aaa86e09db31a3d3f27a66e8bcf6a4ded8
Harley Pig <harleypig@gmail.com>
2010-06-10 13:03:19 -0600

    Put my own particular touch to the code, merged some of the forked code into my own stuff.

--- facebook_bitlbee_rename.pl
+++ facebook_bitlbee_rename.pl
@@ -1,73 +1,109 @@
 # See this script's repository at
-# http://github.com/avar/irssi-bitlbee-facebook-rename for further
-# information.
+# http://github.com/harleypig/irssi-bitlbee-facebook-rename
+# for further information.
+
+# This was forked from http://github.com/avar/irssi-bitlbee-facebook-rename)
+
+# And my original stuff was culled from these places:
+
+# renames bitlbee facebook-via-xmpp buddies to something sane
+# Originally by Tim Angus, http://a.ngus.net/bitlbee_rename.pl
+# Modified slightly to only rename people on chat.facebook.com, and also strip invalid chars from names, by Lakitu7
+# copied in a mod by ajf on #bitlbee to only match u###### names
+# truncates names over 25 chars to comply with bitlbee's limit; thanks Jesper on #bitlbee
+# Fixed FB's change from 'u####' to '-####' - http://pastebin.com/nBS734WH
+
+# Any errors are my own damn fault - harleypig
+
+# script is for irssi. Save it as .irssi/scripts/bitlbee_rename.pl then /script load bitlbee_rename.pl
+# known issues: If the name it's renaming to is already taken, the rename fails.
 
 use strict;
 use warnings;
+
+use Socket; # is this actually used?
 use Irssi;
 use Irssi::Irc;
 use Text::Unidecode;
-use Encode qw(decode);
+use Encode qw( decode );
 
-our $VERSION = '0.02';
+our $VERSION = '0.03-hp';
+
 our %IRSSI = (
-    authors => do { use utf8; 'Ævar Arnfjörð Bjarmason' },
-    contact => 'avarab@gmail.com',
-    name    => 'facebook-bitlbee-rename',
+
+    authors => 'Alan Young',
+    contact => 'harleypig@gmail.com',
+    name    => 'bitlbee-facebook-rename',
     description => 'Rename XMPP chat.facebook.com contacts in bitlbee to human-readable names',
     license => 'GPL',
+
 );
 
-my $bitlbeeChannel = "&bitlbee";
+# These need to be made adjustable
+my $bitlbeeChannel = '&bitlbee';
+my $facebookhostname = 'chat.facebook.com';
+
 my %nicksToRename = ();
 
-sub message_join
-{
+sub message_join {
+
   # "message join", SERVER_REC, char *channel, char *nick, char *address
-  my ($server, $channel, $nick, $address) = @_;
-  my ($username, $host) = split /@/, $address;
+  my ( $server, $channel, $nick, $address ) = @_;
 
-  if ($host eq 'chat.facebook.com' and $channel =~ m/$bitlbeeChannel/ and $nick =~ m/$username/)
-  {
-    $nicksToRename{$nick} = $channel;
-    $server->command("whois -- $nick");
+  my ( $username, $host ) = split /@/, $address, 2;
+
+  if ( $channel =~ /$bitlbeeChannel/
+    && $host    =~ /$facebookhostname/
+    && $nick    =~ /$username/
+    && $nick    =~ /^-\d+/ ) {
+
+    $nicksToRename{ $nick } = $channel;
+    $server->command( "whois -- $nick" );
+
   }
 }
 
-sub whois_data
-{
-  my ($server, $data) = @_;
-  my ($me, $nick, $user, $host) = split(" ", $data);
+sub whois_data {
 
-  if (exists($nicksToRename{$nick}))
-  {
-    my $channel = $nicksToRename{$nick};
-    delete($nicksToRename{$nick});
+  my ( $server, $data ) = @_;
 
-    my $ircname = substr($data, index($data,':')+1);
+  my ( $me, $nick, $user, $host ) = split /\s+/, $data;
 
-    $ircname = munge_nickname( $ircname );
+  if ( exists $nicksToRename{ $nick } ) {
 
-    if ($ircname ne $nick)
-    {
-      $server->command("msg $channel rename $nick $ircname");
-      $server->command("msg $channel save");
-    }
+    my $channel = delete $nicksToRename{ $nick };
+
+    # If I need to use utf8 then uncomment this line and remove the
+    # following lines.
+    # my $ircname = munge_nickname( $data );
+
+    ( my $ircname = $data ) =~ s/^.*?://;
+    $ircname =~ s/[^A-Za-z0-9_]//g;
+    $ircname = substr $ircname, 0, 25;
+
+    if ( $ircname ne $nick ) {
+
+    $server->command("msg $channel rename $nick $ircname");
+    $server->command("msg $channel save");
+
+  }
+
   }
 }
 
-sub munge_nickname
-{
-  my ($nick) = @_;
+sub munge_nickname {
 
-  $nick = decode('utf8', $nick);
+  ( my $nick = +shift ) =~ s/^.*?://;
+
+  $nick = decode( 'utf8', $nick );
   $nick =~ s/[- ]/_/g;
-  $nick = unidecode($nick); 
+  $nick = unidecode( $nick );
   $nick =~ s/[^A-Za-z0-9-]//g;
   $nick = substr $nick, 0, 24;
 
   return $nick;
+
 }
 
 Irssi::signal_add_first 'message join' => 'message_join';
-Irssi::signal_add_first 'event 311' => 'whois_data';
+Irssi::signal_add_first 'event 311'    => 'whois_data';
